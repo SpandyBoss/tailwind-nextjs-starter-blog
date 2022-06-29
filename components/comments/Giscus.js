af0b652214082aab52a83461c8ea8ede72536354
Spandan <spandansehgal@gmail.com>
2022-06-29 13:42:57 +0530

    Update Giscus.js

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -3,6 +3,7 @@ import { useTheme } from 'next-themes'
 
 import siteMetadata from '@/data/siteMetadata'
 
+
 const Giscus = () => {
   const [enableLoadComments, setEnabledLoadComments] = useState(true)
   const { theme, resolvedTheme } = useTheme()
